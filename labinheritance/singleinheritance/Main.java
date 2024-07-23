d441ed74dffb27480f84c93236713fc28e689deb
Runador <runador123456@gmail.com>
2024-07-23 14:27:06 +0300

    Adding randomarraylist

--- labinheritance/singleinheritance/Main.java
+++ labinheritance/singleinheritance/Main.java
@@ -3,9 +3,7 @@ package labinheritance.singleinheritance;
 public class Main {
 
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.eat();
-        dog.bark();
+
     }
 
 }
